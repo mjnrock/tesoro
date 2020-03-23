e0d65ee98bfa02d01fba7dd22e5652e118f0a767
Matt Nawrocki <mjnrock@umich.edu>
2020-03-23 17:27:14 -0400

    init

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import App from "./App";
 
 import MTSLib from "@lespantsfancy/message-transfer-system";
 
-const MTS = (new MTSLib.Main()).loadNetwork(false);
-MTS.Network.createWebSocket({ uri: `localhost:3000` });
+// const MTS = (new MTSLib.Main()).loadNetwork(false);
+// MTS.Network.createWebSocket({ uri: `localhost:3000` });
+const MTS = new MTSLib.Main({ receive: (msg) => {
+    document.getElementById("debug").innerHTML = MTSLib.Helper.CloneAsObject(msg.payload, true);
+} });
+MTS.Router.addRoute(MTS, true);
+
+const [ GEO_LOC ] = MTS.register(new MTSLib.Browser.GeoLocationNode({ name: "GeoLocation", receive: console.log }));
+GEO_LOC.watchPosition();
+
+console.log(MTS);
 
 ReactDOM.render(
     <App />,
